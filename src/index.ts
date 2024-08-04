edb6629e88dd77820d49b09c516921c09abc6266
Av32000 <ae.av32000@gmail.com>
2024-08-04 22:58:40 +0200

    Add Request Logging + /ping

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,14 @@ writeFileSync('/bhome/hello.txt', "Hello World !")
 const PORT = 3000;
 
 const requestListener = (req: IncomingMessage, res: ServerResponse): void => {
+  console.log("[BTest] Incoming request : " + req.method + " " + req.url);
+
   if (req.method === 'GET' && req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World !');
+  } else if (req.method === 'GET' && req.url === '/ping') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Pong !');
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
